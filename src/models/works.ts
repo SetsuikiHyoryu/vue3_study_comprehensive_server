22735cd60a5d7c377d57a8a9f237138b08b282d0
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-02-27 17:20:48 +0900

    docs: 更新注释

--- src/models/works.ts
+++ src/models/works.ts
@@ -1,23 +1,28 @@
 import { Schema, model } from 'mongoose'
 import type { Model, Document } from 'mongoose'
 
+// 数据模型接口
 export interface WorkInterface extends Document {
   title: string
   author: string
 }
 
+// 数据查询参数
 export interface QueryWorksParameters {
   title?: string
   author?: string
 }
 
+// 数据添加参数
 export type AddWorkParameter = WorkInterface
 
+// 数据类型
 const WorkSchema: Schema = new Schema<WorkInterface>({
   title: { type: String, required: true },
   author: { type: String, required: true },
 })
 
+// 数据模型
 const WorkModel: Model<WorkInterface> = model<WorkInterface>('Works', WorkSchema)
 
 export default WorkModel
